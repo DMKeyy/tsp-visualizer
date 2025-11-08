21d936fffa8b71b490a8ca8df9c9d9cffb7de14f
Haiouani Anis <153380893+DMKeyy@users.noreply.github.com>
2025-11-08 21:58:17 +0100

    Enhance Logger functionality: update save_csv method to append results to existing CSV and improve error handling

--- src/Logger.py
+++ src/Logger.py
@@ -1,8 +1,10 @@
 
 import pandas as pd
 
+
 class Logger:
     def __init__(self):
+        # in-memory buffer of results for this run
         self.results = []
 
     def record(self, algo_name, best_distance, best_route, iterations):
@@ -13,12 +15,24 @@ class Logger:
             "Route": best_route
         })
 
-    def save_csv(self, filepath="results.csv"):
-        df = pd.DataFrame(self.results)
-        df.to_csv(filepath, index=False)
+    def save_csv(self, filepath):
+
+        df_new = pd.DataFrame(self.results)
+
+
+        try:
+            df_existing = pd.read_csv(filepath)
+            df_combined = pd.concat([df_existing, df_new], ignore_index=True)
+        except (FileNotFoundError, pd.errors.EmptyDataError):
+            df_combined = df_new
+
+
+        df_combined.to_csv(filepath, index=False)
+
+        self.results = []
         print(f"Results saved to {filepath}")
 
-    def show_summary(self,filepath):
+    def show_summary(self, filepath):
         pd.set_option('display.max_colwidth', None)
         df = pd.read_csv(filepath)
         print("Summary of Results:")
